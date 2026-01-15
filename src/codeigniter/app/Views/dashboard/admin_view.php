3dc8a7fa7c74700746486bae45812c26028bd330
hujan <alif.fitra1511@gmail.com>
2026-01-15 15:04:18 +0700

    fix: memperbaiki struktur dashboard

--- src/codeigniter/app/Views/dashboard/admin_view.php
+++
@@ -1,6 +0,0 @@
-<?= $this->extend('layouts/template') ?>
-
-<?= $this->section('content') ?>
-<h1>Admin Dashboard</h1>
-<p>Welcome to the admin dashboard.</p>
-<?= $this->endSection() ?>
--- src/codeigniter/app/Views/dashboard/fakultas_view.php
+++
@@ -1,6 +0,0 @@
-<?= $this->extend('layouts/template') ?>
-
-<?= $this->section('content') ?>
-<h1>Fakultas Dashboard</h1>
-<p>Welcome to the fakultas dashboard.</p>
-<?= $this->endSection() ?>
